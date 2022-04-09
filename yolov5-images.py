3316b60469f05efb33169b0e81cfc777ff109915
tomaszkuehn <tomasz.kuehn@gmail.com>
2022-04-09 20:44:41 +0200

    Ignoring missing src images with try

--- yolov5-images.py
+++ yolov5-images.py
@@ -1,6 +1,6 @@
 # IP camera image capture (player) OpenCV - all in memory
 # Apache 2.0 license
-# Copyright (C) 2022 Tomasz Kuehn v0.2
+# Copyright (C) 2022 Tomasz Kuehn v0.3
 
 import cv2
 import torch
@@ -15,11 +15,12 @@ from threading import Thread
 def thread_image():
     global r
     #r = requests.get("http://192.168.0.250/cgi-bin/jpg/image.cgi", stream = False, auth = HTTPBasicAuth("Admin", "1234"))
-    r = requests.get("http://192.168.88.182:8080/shot.jpg", stream = True)   
+    #r = requests.get("http://192.168.88.209:8080/shot.jpg", stream = False) 
+    r = requests.get("http://192.168.88.182:8080/shot.jpg", stream = False) 
 
 
 # Model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', device='0') #device 0 is Jetson cuda device
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained = 'True', device='0') #device 0 is Jetson cuda device
 
 model.conf = 0.25  # NMS confidence threshold
 model.iou = 0.45  # NMS IoU threshold
@@ -31,37 +32,45 @@ model.amp = False  # Automatic Mixed Precision (AMP) inference
 
 #model.cuda()
 fpsstart = 0
-color = (255, 155, 155)
+color = (255, 255, 255)
 
+image = np.zeros(shape=[360, 640, 3], dtype=np.uint8)
+cv2.imshow("Image", image)
+cv2.waitKey(2000)
 t = Thread(target = thread_image)
 t.start()
 while 1:
-    tt = time.time()
+    #tt = time.time()
     t.join()
-    image = cv2.imdecode(np.frombuffer(r.content, dtype=np.uint8), cv2.IMREAD_ANYCOLOR)
+    r1 = r
     t = Thread(target = thread_image)
     t.start()
-    #points = (960, 540)
-    #image = cv2.resize(image, points, interpolation= cv2.INTER_LINEAR)
-    diff = time.time() - tt
-    print("[INFO] IMAGE took {:.6f} seconds".format(diff))
-    tt = time.time()
-    # Inference
-    results = model(image, size=640)  # includes NMS
-    diff = time.time() - tt
-    print("[INFO] YOLO took {:.6f} seconds".format(diff))
-    tt = time.time()
-    # Results
-    #results.print()  
-    results.render() #render boxes around objects
-    diff = time.time() - fpsstart
-    fpsstart = time.time()
-    fps = 1.0 / diff
-    cv2.putText(results.imgs[0], 'FPS ' + '%.2f' % fps, (20, 20), cv2.FONT_HERSHEY_PLAIN, 1.0, color, 2)
-    cv2.imshow("Image", results.imgs[0])
-    cv2.waitKey(1)
-    diff = time.time() - tt
-    print("[INFO] DISP took {:.6f} seconds".format(diff))
+    try:
+        image = cv2.imdecode(np.frombuffer(r1.content, dtype=np.uint8), cv2.IMREAD_ANYCOLOR) 
+        #points = (320, 240)
+        #image = cv2.resize(image, points, interpolation= cv2.INTER_LINEAR)
+        #diff = time.time() - tt
+        #print("[INFO] IMAGE took {:.6f} seconds".format(diff))
+
+        tt = time.time()
+        # Inference
+        results = model(image, size=320)  # includes NMS
+        diff = time.time() - tt
+        print("[INFO] YOLO took {:.6f} seconds".format(diff))
+    
+        # Results
+        #results.print()  
+        results.render() #render boxes around objects
+        diff = time.time() - fpsstart
+        fpsstart = time.time()
+        fps = 1.0 / diff
+        #print("FPS: %.2f" % fps)
+        
+        cv2.putText(results.imgs[0], 'FPS ' + '%.2f' % fps, (20, 20), cv2.FONT_HERSHEY_PLAIN, 1.5, color, 2)
+        cv2.imshow("Image", results.imgs[0])
+        cv2.waitKey(1)
+    except:
+        print("No image")
 
 #results.xyxy[0]  # im1 predictions (tensor)
 #results.pandas().xyxy[0]  # im1 predictions (pandas)
